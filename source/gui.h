2ad70e3a79ee14424410896eb0ef61683b60997b
Idler_pl <selppkez@gmail.com>
2025-04-23 13:15:25 +0200

    find items improvments
    search results allow for batch paging to find all iterations of item search or range

--- source/gui.h
+++ source/gui.h
@@ -213,6 +213,11 @@ public:
 	SearchResultWindow* GetSearchWindow();
 	SearchResultWindow* ShowSearchWindow();
 	void HideSearchWindow();
+	
+	// Search state persistence
+	void StoreSearchState(uint16_t itemId, bool onSelection);
+	void RestoreSearchState(SearchResultWindow* window);
+	bool HasStoredSearch() const { return has_last_search; }
 
 	// Minimap
 	void CreateMinimap();
@@ -508,6 +513,13 @@ public:
 
 	// Dark mode
 	void ApplyDarkMode();
+
+	// Search state variables
+	bool has_last_search;
+	uint16_t last_search_itemid;
+	bool last_search_on_selection;
+	wxString last_ignored_ids_text;
+	bool last_ignored_ids_enabled;
 };
 
 extern GUI g_gui;
